0c1c3b39828b5460d2c0aa05fc06c00846a07379
ivovka <vladimir_iva@pisem.net>
2012-03-29 10:47:39 +0400

    kmod: install tools

--- package/kmod/kmod.mk
+++ package/kmod/kmod.mk
@@ -13,6 +13,19 @@ KMOD_SECTION = system
 KMOD_DESCRIPTION = The new module loader
 KMOD_OPKG_DEPENDENCIES = libc
 KMOD_CONF_OPT = \
-	--disable-tools
+	--enable-tools \
+	--enable-logging \
+	--without-xz \
+	--without-zlib
 
+define KMOD_OPKG_TOOLS
+  mkdir -p $(BUILD_DIR_OPKG)/$(KMOD_BASE_NAME)/sbin
+  ln -sf /usr/bin/kmod $(BUILD_DIR_OPKG)/$(KMOD_BASE_NAME)/sbin/lsmod
+  ln -sf /usr/bin/kmod $(BUILD_DIR_OPKG)/$(KMOD_BASE_NAME)/sbin/insmod
+  ln -sf /usr/bin/kmod $(BUILD_DIR_OPKG)/$(KMOD_BASE_NAME)/sbin/rmmod
+  ln -sf /usr/bin/kmod $(BUILD_DIR_OPKG)/$(KMOD_BASE_NAME)/sbin/modinfo
+  ln -sf /usr/bin/kmod $(BUILD_DIR_OPKG)/$(KMOD_BASE_NAME)/sbin/modprobe
+endef
+
+KMOD_PRE_BUILD_OPKG_HOOKS += KMOD_OPKG_TOOLS
 $(eval $(call AUTOTARGETS,package,kmod))
