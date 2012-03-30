88f738d144bbcbb877980925b13eb9a41fbca564
Vladimir Ivakin <vladimir_iva@pisem.net>
2012-03-31 01:03:37 +0400

    jasper: remove binaries from opk

--- package/jasper/jasper.mk
+++ package/jasper/jasper.mk
@@ -12,8 +12,12 @@ JASPER_INSTALL_TARGET = YES
 JASPER_BUILD_OPKG = YES
 
 JASPER_SECTION = graphics
-JASPER_PRIORITY = optional
-JASPER_MAINTAINER = Vladimir Ivakin vladimir_iva@pisem.net
 JASPER_DESCRIPTION = JPEG-2000 Part-1 standard (i.e., ISO/IEC 15444-1) implementation
+
+define JASPER_OPKG_RM_BIN
+	rm -rf $(BUILD_DIR_OPKG)/$(JASPER_BASE_NAME)/usr/bin
+endef
+
+JASPER_PRE_BUILD_OPKG_HOOKS += JASPER_OPKG_RM_BIN
 
 $(eval $(call AUTOTARGETS,package,jasper))
