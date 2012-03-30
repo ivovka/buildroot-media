35778f9f610172256b6ee75dfdce8b041b36abaa
Vladimir Ivakin <vladimir_iva@pisem.net>
2012-03-31 01:05:55 +0400

    lzo: bump version

--- package/lzo/lzo.mk
+++ package/lzo/lzo.mk
@@ -3,14 +3,12 @@
 # lzo
 #
 #############################################################
-LZO_VERSION = 2.05
+LZO_VERSION = 2.06
 LZO_SITE = http://www.oberhumer.com/opensource/lzo/download
 LZO_INSTALL_STAGING = YES
 LZO_BUILD_OPKG = YES
 
 LZO_SECTION = compress
-LZO_PRIORITY = optional
-LZO_MAINTAINER = Vladimir Ivakin vladimir_iva@pisem.net
 LZO_DESCRIPTION = LZO data compressor
 
 $(eval $(call AUTOTARGETS,package,lzo))
