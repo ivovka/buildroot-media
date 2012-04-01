7452b4932f473fc2b3def152f505f76af56cc176
Vladimir Ivakin <vladimir_iva@pisem.net>
2012-04-01 23:30:19 +0400

    yajl: bump version

--- package/yajl/yajl.mk
+++ package/yajl/yajl.mk
@@ -4,14 +4,18 @@
 #
 ################################################################################
 
-YAJL_VERSION = 2.0.2
+YAJL_VERSION = 2.0.4
 YAJL_SITE = git://github.com/lloyd/yajl.git
 YAJL_INSTALL_STAGING = YES
 YAJL_BUILD_OPKG = YES
 
 YAJL_SECTION = text
-YAJL_PRIORITY = optional
-YAJL_MAINTAINER = Vladimir Ivakin vladimir_iva@pisem.net
 YAJL_DESCRIPTION = Yet Another JSON Library
+
+define YAJL_OPKG_RM_BIN
+	rm -rf $(BUILD_DIR_OPKG)/$(YAJL_BASE_NAME)/usr/{bin,share}
+endef
+
+YAJL_PRE_BUILD_OPKG_HOOKS += YAJL_OPKG_RM_BIN
 
 $(eval $(call CMAKETARGETS,package,yajl))
