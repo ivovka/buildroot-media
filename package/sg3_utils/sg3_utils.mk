e35370217c7c2690dbfec21d7f8323bb1611f5e4
ivovka <vladimir_iva@pisem.net>
2012-04-02 11:34:08 +0400

    sg3-utils: bump version

--- package/sg3_utils/sg3_utils.mk
+++ package/sg3_utils/sg3_utils.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SG3_UTILS_VERSION = 1.30
+SG3_UTILS_VERSION = 1.32
 SG3_UTILS_SITE = http://sg.danny.cz/sg/p
 SG3_UTILS_SOURCE = sg3_utils-$(SG3_UTILS_VERSION).tar.bz2
 SG3_UTILS_INSTALL_STAGING = YES
