00e4d69dbb40bc88b918f807705ab098721bd0c4
Vladimir Ivakin <vladimir_iva@pisem.net>
2012-04-02 23:16:30 +0400

    gmp: add opk build instructions; bump version

--- package/gmp/gmp.mk
+++ package/gmp/gmp.mk
@@ -4,10 +4,16 @@
 #
 #############################################################
 
-GMP_VERSION = 5.0.1
+GMP_VERSION = 5.0.4
 GMP_SITE = $(BR2_GNU_MIRROR)/gmp
 GMP_SOURCE = gmp-$(GMP_VERSION).tar.bz2
 GMP_INSTALL_STAGING = YES
+GMP_BUILD_OPKG = YES
+
+GMP_SECTION = devel
+GMP_DESCRIPTION = GNU Multiple Precision Arithmetic Library
+
+GMP_CONF_OPT = --disable-mpbsd --disable-cxx
 
 $(eval $(call AUTOTARGETS,package,gmp))
 $(eval $(call AUTOTARGETS,package,gmp,host))
