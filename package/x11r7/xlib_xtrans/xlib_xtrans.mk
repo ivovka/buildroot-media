bed1ee45e8dbf9bfcf34af9403c60c9da160e580
Vladimir Ivakin <vladimir_iva@pisem.net>
2012-04-02 23:23:45 +0400

    xlib_xtrans: bump version

--- package/x11r7/xlib_xtrans/xlib_xtrans.mk
+++ package/x11r7/xlib_xtrans/xlib_xtrans.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_XTRANS_VERSION = 1.2.6
+XLIB_XTRANS_VERSION = 1.2.7
 XLIB_XTRANS_SOURCE = xtrans-$(XLIB_XTRANS_VERSION).tar.bz2
 XLIB_XTRANS_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_XTRANS_AUTORECONF = NO
@@ -15,6 +15,12 @@ XLIB_XTRANS_DEPENDENCIES = xutil_util-macros
 XLIB_XTRANS_NAME_OPKG = xtrans
 XLIB_XTRANS_SECTION = x11
 XLIB_XTRANS_DESCRIPTION = Abstract network code for X
+
+define XLIB_XTRANS_OPKG_RM_PKGCONF
+	rm -rf $(BUILD_DIR_OPKG)/$(XLIB_XTRANS_BASE_NAME)/usr/share/pkgconfig
+endef
+
+XLIB_XTRANS_PRE_BUILD_OPKG_HOOKS += XLIB_XTRANS_OPKG_RM_PKGCONF
 
 $(eval $(call AUTOTARGETS,package/x11r7,xlib_xtrans))
 $(eval $(call AUTOTARGETS,package/x11r7,xlib_xtrans,host))
