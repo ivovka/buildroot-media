cfc7d1bb64793b8d7b6bde23c352af86f8f6e77d
ivovka <vladimir_iva@pisem.net>
2012-04-02 11:27:37 +0400

    libgpg-error: bump version, rm binaries from opk

--- package/libgpg-error/libgpg-error.mk
+++ package/libgpg-error/libgpg-error.mk
@@ -3,7 +3,7 @@
 # libgpg-error
 #
 #############################################################
-LIBGPG_ERROR_VERSION:=1.8
+LIBGPG_ERROR_VERSION:=1.10
 LIBGPG_ERROR_SOURCE:=libgpg-error-$(LIBGPG_ERROR_VERSION).tar.bz2
 LIBGPG_ERROR_SITE:=ftp://gd.tuwien.ac.at/privacy/gnupg/libgpg-error
 
@@ -11,8 +11,12 @@ LIBGPG_ERROR_INSTALL_STAGING = YES
 LIBGPG_ERROR_BUILD_OPKG = YES
 
 LIBGPG_ERROR_SECTION = security
-LIBGPG_ERROR_PRIORITY = optional
-LIBGPG_ERROR_MAINTAINER = Vladimir Ivakin vladimir_iva@pisem.net
 LIBGPG_ERROR_DESCRIPTION = Library that defines common error values for GnuPG components
+
+define LIBGPG_ERROR_OPKG_RM_BIN
+  rm -rf $(BUILD_DIR_OPKG)/$(LIBGPG_ERROR_BASE_NAME)/usr/{bin,share}
+endef
+
+LIBGPG_ERROR_PRE_BUILD_OPKG_HOOKS += LIBGPG_ERROR_OPKG_RM_BIN
 
 $(eval $(call AUTOTARGETS,package,libgpg-error))
