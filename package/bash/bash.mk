48070b53b321df8de712897e6bc85688d277a589
ivovka <vladimir_iva@pisem.net>
2012-04-04 08:39:41 +0400

    bash: bump version, put patches for the new version

--- package/bash/bash.mk
+++ package/bash/bash.mk
@@ -4,14 +4,33 @@
 #
 #############################################################
 
-BASH_VERSION = 4.1
+BASH_VERSION = 4.2
 BASH_SITE = $(BR2_GNU_MIRROR)/bash
 BASH_DEPENDENCIES = ncurses
+BASH_BUILD_OPKG = YES
+BASH_SECTION = system
+BASH_PRIORITY = important
+BASH_DESCRIPTION = Bourne Again Shell
 
 # Make sure we build after busybox so that /bin/sh links to bash
-ifeq ($(BR2_PACKAGE_BUSYBOX),y)
-BASH_DEPENDENCIES += busybox
-endif
+BASH_OPKG_DEPENDENCIES = ncurses
+
+define BASH_APPLY_UPSTREAM_PATCHES
+  for patch in `ls $(BASH_DIR_PREFIX)/$(BASH_NAME)/upstream.patches/*.patch`; do \
+    cat $$patch | patch -d $(BASH_DIR) -p0; \
+  done
+endef
+
+BASH_POST_EXTRACT_HOOKS += BASH_APPLY_UPSTREAM_PATCHES
+
+BASH_CONF_ENV += bash_cv_job_control_missing=present
+BASH_CONF_OPT += \
+  --bindir=/bin \
+  --enable-job-control \
+  --without-bash-malloc \
+  --without-installed-readline \
+  --disable-nls \
+  --disable-rpath
 
 # Save the old sh file/link if there is one and symlink bash->sh
 define BASH_INSTALL_TARGET_CMDS
@@ -34,4 +53,10 @@ define BASH_UNINSTALL_TARGET_CMDS
 	fi
 endef
 
+define BASH_OPKG_LN
+  rm -f $(BUILD_DIR_OPKG)/$(BASH_BASE_NAME)/bin/bashbug
+  ln -sf bash $(BUILD_DIR_OPKG)/$(BASH_BASE_NAME)/bin/sh
+endef
+
+BASH_PRE_BUILD_OPKG_HOOKS += BASH_OPKG_LN
 $(eval $(call AUTOTARGETS,package,bash))
