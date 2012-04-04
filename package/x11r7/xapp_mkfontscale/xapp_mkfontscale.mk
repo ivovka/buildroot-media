623d70569c903189bd508063c855d627cb7961aa
ivovka <vladimir_iva@pisem.net>
2012-04-04 08:46:59 +0400

    xapp_mkfontscale: bump version, fix opkg dependencies

--- package/x11r7/xapp_mkfontscale/xapp_mkfontscale.mk
+++ package/x11r7/xapp_mkfontscale/xapp_mkfontscale.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_MKFONTSCALE_VERSION = 1.0.8
+XAPP_MKFONTSCALE_VERSION = 1.1.0
 XAPP_MKFONTSCALE_SOURCE = mkfontscale-$(XAPP_MKFONTSCALE_VERSION).tar.bz2
 XAPP_MKFONTSCALE_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_MKFONTSCALE_AUTORECONF = NO
@@ -12,7 +12,7 @@ XAPP_MKFONTSCALE_BUILD_OPKG = YES
 XAPP_MKFONTSCALE_NAME_OPKG = mkfontscale
 XAPP_MKFONTSCALE_SECTION = x11
 XAPP_MKFONTSCALE_DESCRIPTION = create an index of scalable font files for X
-XAPP_MKFONTSCALE_OPKG_DEPENDENCIES = freetype,libfontenc
+XAPP_MKFONTSCALE_OPKG_DEPENDENCIES = zlib,freetype,libfontenc
 XAPP_MKFONTSCALE_DEPENDENCIES = zlib freetype xlib_libfontenc xproto_xproto
 HOST_XAPP_MKFONTSCALE_DEPENDENCIES = host-zlib host-freetype host-xlib_libfontenc host-xproto_xproto
 
