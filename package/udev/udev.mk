ddd6cd16a00a17585899ac00d687ba97eead737a
ivovka <vladimir_iva@pisem.net>
2012-04-06 15:45:06 +0400

    udev: bump version, fix dependencies, fix config options

--- package/udev/udev.mk
+++ package/udev/udev.mk
@@ -3,19 +3,16 @@
 # udev
 #
 #############################################################
-UDEV_VERSION:=181
+UDEV_VERSION:=182
 UDEV_SOURCE:=udev-$(UDEV_VERSION).tar.bz2
 UDEV_SITE:=$(BR2_KERNEL_MIRROR)/linux/utils/kernel/hotplug/
 UDEV_INSTALL_STAGING=YES
 UDEV_BUILD_OPKG = YES
+UDEV_DEPENDENCIES += libglib2 pciutils usbutils libusb-compat util-linux kmod
+UDEV_OPKG_DEPENDENCIES = libglib2,pciutils,usbutils,libusb-compat,kmod,util-linux
 
 UDEV_SECTION = system
-UDEV_PRIORITY = optional
-UDEV_MAINTAINER = Vladimir Ivakin vladimir_iva@pisem.net
 UDEV_DESCRIPTION = A Userspace Implementation for dynamic dev nodes
-UDEV_OPKG_DEPENDENCIES = libglib2,pciutils,usbutils,libusb-compat,kmod
-
-UDEV_DEPENDENCIES += libglib2 pciutils usbutils libusb-compat kmod
 
 UDEV_CONF_ENV += \
     ac_cv_file__usr_share_pci_ids="yes" \
@@ -24,21 +21,25 @@ UDEV_CONF_ENV += \
     LDFLAGS="$(TARGET_LDFLAGS) -I$(STAGING_DIR)/usr/include/glib-2.0"
 
 UDEV_CONF_OPT += \
-    --libexecdir=/lib/udev \
+    --libexecdir=/lib \
+    --with-firmware-path=/lib/firmware \
+    --disable-manpages \
     --disable-debug \
     --disable-logging \
     --disable-rule_generator \
-    --disable-hwdb \
-    --disable-udev_acl \
     --enable-gudev \
     --disable-introspection \
     --disable-keymap \
     --disable-floppy \
-    --disable-edd \
     --without-selinux \
     --without-systemdsystemunitdir \
-    --with-firmware-path=/lib/firmware \
-    --with-pci-ids-path=/usr/share/pci.ids \
-    --with-usb-ids-path=/usr/share/usb.ids
+    --with-pci-ids-path=/usr/share/pci.ids.gz \
+    --with-usb-ids-path=/usr/share/usb.ids.gz
+
+define UDEV_OPKG_CLEANUP
+  rm -rf $(BUILD_DIR_OPKG)/$(UDEV_BASE_NAME)/usr/share
+endef
+
+UDEV_PRE_BUILD_OPKG_HOOKS += UDEV_OPKG_CLEANUP
 
 $(eval $(call AUTOTARGETS,package,udev))
