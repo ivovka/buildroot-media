498bf8cafe6bf2c6983cf3172957e81678684e94
ivovka <vladimir_iva@pisem.net>
2012-04-06 15:23:20 +0400

    libvorbis: bump version, cleanup build script

--- package/multimedia/libvorbis/libvorbis.mk
+++ package/multimedia/libvorbis/libvorbis.mk
@@ -4,22 +4,19 @@
 #
 #############################################################
 
-LIBVORBIS_VERSION = 1.3.2
+LIBVORBIS_VERSION = 1.3.3
 LIBVORBIS_SOURCE = libvorbis-$(LIBVORBIS_VERSION).tar.gz
 LIBVORBIS_SITE = http://downloads.xiph.org/releases/vorbis/$(LIBVORBIS-SOURCE)
 LIBVORBIS_AUTORECONF = YES
 LIBVORBIS_INSTALL_STAGING = YES
 LIBVORBIS_INSTALL_TARGET = YES
 LIBVORBIS_BUILD_OPKG = YES
-
-LIBVORBIS_SECTION = libs
-LIBVORBIS_PRIORITY = optional
-LIBVORBIS_MAINTAINER = Vladimir Ivakin vladimir_iva@pisem.net
-LIBVORBIS_DESCRIPTION = Lossless audio compression tools using the ogg-vorbis algorithms
+LIBVORBIS_DEPENDENCIES = host-pkg-config libogg
 LIBVORBIS_OPKG_DEPENDENCIES = libogg
 
-LIBVORBIS_CONF_OPT = --with-ogg=$(STAGING_DIR)/usr
+LIBVORBIS_SECTION = libs
+LIBVORBIS_DESCRIPTION = Lossless audio compression tools using the ogg-vorbis algorithms
 
-LIBVORBIS_DEPENDENCIES = host-pkg-config libogg
+LIBVORBIS_CONF_OPT = --with-ogg=$(STAGING_DIR)/usr
 
 $(eval $(call AUTOTARGETS,package/multimedia,libvorbis))
