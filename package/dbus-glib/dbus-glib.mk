59a10319be4bcc6c1470ebc36055b0efb672e5ab
ivovka <vladimir_iva@pisem.net>
2012-04-06 14:23:33 +0400

    dbus-glib: bump version, cleanup script, fix dependencies

--- package/dbus-glib/dbus-glib.mk
+++ package/dbus-glib/dbus-glib.mk
@@ -3,7 +3,7 @@
 # dbus-glib
 #
 #############################################################
-DBUS_GLIB_VERSION = 0.94
+DBUS_GLIB_VERSION = 0.98
 DBUS_GLIB_SOURCE = dbus-glib-$(DBUS_GLIB_VERSION).tar.gz
 DBUS_GLIB_SITE = http://dbus.freedesktop.org/releases/dbus-glib/
 DBUS_GLIB_INSTALL_STAGING = YES
@@ -19,11 +19,8 @@ DBUS_GLIB_CONF_ENV = ac_cv_have_abstract_sockets=yes \
 DBUS_GLIB_CONF_OPT = --localstatedir=/var \
 		--program-prefix="" \
 		--disable-tests \
-		--disable-xml-docs \
 		--with-introspect-xml=$(DBUS_HOST_INTROSPECT) \
 		--with-dbus-binding-tool=$(DBUS_GLIB_HOST_BINARY) \
-		--disable-bash-completion \
-		--disable-doxygen-docs \
 		--enable-asserts=no
 
 DBUS_GLIB_DEPENDENCIES = host-pkg-config dbus host-dbus host-dbus-glib libglib2 expat
@@ -36,6 +33,13 @@ HOST_DBUS_GLIB_CONF_OPT = \
 		--disable-bash-completion \
 		--disable-doxygen-docs \
 		--enable-asserts=yes
+
+define DBUS_GLIB_OPKG_CLEANUP
+  rm -rf $(BUILD_DIR_OPKG)/$(DBUS_GLIB_BASE_NAME)/etc
+  rm -rf $(BUILD_DIR_OPKG)/$(DBUS_GLIB_BASE_NAME)/usr/{bin,libexec}
+endef
+
+DBUS_GLIB_PRE_BUILD_OPKG_HOOKS += DBUS_GLIB_OPKG_CLEANUP
 
 $(eval $(call AUTOTARGETS,package,dbus-glib))
 $(eval $(call AUTOTARGETS,package,dbus-glib,host))
