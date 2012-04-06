397f15ce69ec97ab6402cb09eee81b721f71047a
ivovka <vladimir_iva@pisem.net>
2012-04-06 15:46:03 +0400

    usbutils: cosmetic changes

--- package/usbutils/usbutils.mk
+++ package/usbutils/usbutils.mk
@@ -6,18 +6,17 @@
 
 USBUTILS_VERSION = 004
 USBUTILS_SITE = http://www.mirrorservice.org/sites/ftp.kernel.org/pub/linux/utils/usb/usbutils
-USBUTILS_DEPENDENCIES = host-pkg-config libusb libusb-compat
 USBUTILS_INSTALL_STAGING = YES
 USBUTILS_BUILD_OPKG = YES
+USBUTILS_DEPENDENCIES = host-pkg-config libusb libusb-compat
+USBUTILS_OPKG_DEPENDENCIES = libusb-compat
 
 USBUTILS_SECTION = system
-USBUTILS_PRIORITY = optional
-USBUTILS_MAINTAINER = Vladimir Ivakin vladimir_iva@pisem.net
 USBUTILS_DESCRIPTION = Linux USB Utilities
-USBUTILS_OPKG_DEPENDENCIES = libusb-compat,zlib
 
 ifeq ($(BR2_PACKAGE_USBUTILS_ZLIB),y)
 	USBUTILS_DEPENDENCIES += zlib
+	USBUTILS_OPKG_DEPENDENCIES += ,zlib
 else
 	USBUTILS_CONF_OPT = --disable-zlib
 endif
@@ -28,33 +27,39 @@ define USBUTILS_TARGET_CLEANUP
 	rm -f $(TARGET_DIR)/usr/share/pkgconfig/usbutils.pc
 endef
 
+define USBUTILS_OPKG_CLEANUP
+	rm -f $(BUILD_DIR_OPKG)/$(USBUTILS_BASE_NAME)/usr/bin/{lsusb.py,usb-devices,usbhid-dump}
+	rm -rf $(BUILD_DIR_OPKG)/$(USBUTILS_BASE_NAME)/usr/sbin
+	rm -rf $(BUILD_DIR_OPKG)/$(USBUTILS_BASE_NAME)/usr/share/pkgconfig
+endef
+
 USBUTILS_POST_INSTALL_TARGET_HOOKS += USBUTILS_TARGET_CLEANUP
-
-define USBUTILS_REMOVE_UNCOMPRESSED_IDS
-	rm -f $(TARGET_DIR)/usr/share/usb.ids
-endef
-
-define USBUTILS_REMOVE_COMPRESSED_IDS
-	rm -f $(TARGET_DIR)/usr/share/usb.ids.gz
-endef
+USBUTILS_PRE_BUILD_OPKG_HOOKS += USBUTILS_OPKG_CLEANUP
 
 ifeq ($(BR2_PACKAGE_USBUTILS_ZLIB),y)
-USBUTILS_POST_INSTALL_TARGET_HOOKS += USBUTILS_REMOVE_UNCOMPRESSED_IDS
+define USBUTILS_RM_IDS
+	rm -f $(1)/usr/share/usb.ids
+endef
 else
-USBUTILS_POST_INSTALL_TARGET_HOOKS += USBUTILS_REMOVE_COMPRESSED_IDS
+define USBUTILS_RM_IDS
+	rm -f $(1)/usr/share/usb.ids.gz
+endef
 endif
+
+define USBUTILS_RM_IDS_TARGET
+  $(call USBUTILS_RM_IDS,$(TARGET_DIR))
+endef
+
+define USBUTILS_RM_IDS_OPKG
+  $(call USBUTILS_RM_IDS,$(BUILD_DIR_OPKG)/$(USBUTILS_BASE_NAME))
+endef
+
+USBUTILS_POST_INSTALL_TARGET_HOOKS += USBUTILS_RM_IDS_TARGET
+USBUTILS_PRE_BUILD_OPKG_HOOKS += USBUTILS_RM_IDS_OPKG
 
 define USBUTILS_REMOVE_DEVFILES
 	rm -f $(TARGET_DIR)/usr/bin/libusb-config
 endef
-
-define USBUTILS_COPY_IDS
-	mkdir -p $(BUILD_DIR_OPKG)/usbutils-$(USBUTILS_VERSION)/usr/share
-	cp $(TOPDIR)/package/usbutils/usb.ids $(BUILD_DIR_OPKG)/usbutils-$(USBUTILS_VERSION)/usr/share
-	rm -rf $(BUILD_DIR_OPKG)/usbutils-$(USBUTILS_VERSION)/usr/share/pkgconfig
-endef
-
-USBUTILS_PRE_BUILD_OPKG_HOOKS += USBUTILS_COPY_IDS
 
 ifneq ($(BR2_HAVE_DEVFILES),y)
 USBUTILS_POST_INSTALL_TARGET_HOOKS += USBUTILS_REMOVE_DEVFILES
