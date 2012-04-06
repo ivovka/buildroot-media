52d22c5b1b274df1a5861ffbef181fa5f6cc4503
ivovka <vladimir_iva@pisem.net>
2012-04-06 14:40:44 +0400

    freetype: cosmetic

--- package/freetype/freetype.mk
+++ package/freetype/freetype.mk
@@ -9,20 +9,16 @@ FREETYPE_SOURCE = freetype-$(FREETYPE_VERSION).tar.bz2
 FREETYPE_INSTALL_STAGING = YES
 FREETYPE_INSTALL_TARGET = YES
 FREETYPE_BUILD_OPKG = YES
+# Make freetype depends on host-freetype, because I am not sure wich packages really use freetype on host
+FREETYPE_DEPENDENCIES = host-pkg-config host-freetype zlib
+FREETYPE_OPKG_DEPENDENCIES = zlib
+HOST_FREETYPE_DEPENDENCIES = host-pkg-config
 
 FREETYPE_SECTION = print
-FREETYPE_PRIORITY = optional
-FREETYPE_MAINTAINER = Vladimir Ivakin vladimir_iva@pisem.net
 FREETYPE_DESCRIPTION = TrueType font rendering library
-FREETYPE_OPKG_DEPENDENCIES = zlib
 
+FREETYPE_CONF_OPT = --with-zlib
 FREETYPE_MAKE_OPT = CCexe="$(HOSTCC)"
-FREETYPE_DEPENDENCIES = host-pkg-config $(if $(BR2_PACKAGE_ZLIB),zlib)
-
-# Make freetype depends on host-freetype, because I am not sure wich packages really use freetype on host
-FREETYPE_DEPENDENCIES += host-freetype
-
-HOST_FREETYPE_DEPENDENCIES = host-pkg-config
 
 define FREETYPE_FREETYPE_CONFIG_STAGING_FIXUP
 	$(SED) "s,^prefix=.*,prefix=\'$(STAGING_DIR)/usr\',g" \
