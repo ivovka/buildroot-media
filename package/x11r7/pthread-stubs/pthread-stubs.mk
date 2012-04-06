94fbad9c274815dd693b58dc81c9249946afdef2
ivovka <vladimir_iva@pisem.net>
2012-04-06 15:46:59 +0400

    pthread-stubs: do not build opkg, because nothing to install on target

--- package/x11r7/pthread-stubs/pthread-stubs.mk
+++ package/x11r7/pthread-stubs/pthread-stubs.mk
@@ -8,7 +8,7 @@ PTHREAD_STUBS_SOURCE = libpthread-stubs-$(PTHREAD_STUBS_VERSION).tar.bz2
 PTHREAD_STUBS_SITE = http://xcb.freedesktop.org/dist/
 
 PTHREAD_STUBS_INSTALL_STAGING = YES
-PTHREAD_STUBS_BUILD_OPKG = YES
+PTHREAD_STUBS_BUILD_OPKG = NO
 
 PTHREAD_STUBS_SECTION = devel
 PTHREAD_STUBS_DESCRIPTION = A library providing weak aliases for pthread functions
