6363e98c655d1394f60dff6c16f51b787936acc8
ivovka <vladimir_iva@pisem.net>
2012-04-10 17:51:46 +0400

    gnutls:bump version; fix dependencies. Workaround for documentation build error

--- package/gnutls/gnutls.mk
+++ package/gnutls/gnutls.mk
@@ -4,24 +4,56 @@
 #
 #############################################################
 
-GNUTLS_VERSION = 2.10.5
-GNUTLS_SOURCE = gnutls-$(GNUTLS_VERSION).tar.bz2
+GNUTLS_VERSION = 3.0.7
+GNUTLS_SOURCE = gnutls-$(GNUTLS_VERSION).tar.xz
 GNUTLS_SITE = http://ftp.gnu.org/gnu/gnutls/
-GNUTLS_DEPENDENCIES = libgcrypt
-GNUTLS_CONF_OPT += --without-libgcrypt-prefix
 GNUTLS_INSTALL_STAGING = YES
 GNUTLS_BUILD_OPKG = YES
-
+GNUTLS_DEPENDENCIES = cryptodev-linux zlib nettle
+GNUTLS_OPKG_DEPENDENCIES = cryptodev-linux,zlib,nettle
 GNUTLS_SECTION = security
-GNUTLS_PRIORITY = optional
-GNUTLS_MAINTAINER = Vladimir Ivakin vladimir_iva@pisem.net
 GNUTLS_DESCRIPTION = Development Library for TLS applications
-GNUTLS_OPKG_DEPENDENCIES = lzo,libgcrypt
 
 GNUTLS_CONF_OPT += \
-            --with-included-libtasn1 \
-            --with-lzo \
-            --with-libgcrypt \
-            --disable-nls
+  --enable-hardware-acceleration \
+  --enable-cryptodev \
+  --enable-openssl-compatibility \
+  --without-p11-kit \
+  --with-included-libtasn1 \
+  --with-included-libcfg \
+  --disable-nls \
+  --disable-guile \
+  --disable-valgrind-tests \
+  --with-libnettle-prefix="$(STAGING_DIR)/usr" \
+  --with-libz-prefix="$(STAGING_DIR)/usr" \
+  --disable-gtk-doc \
+  --disable-gtk-doc-html \
+  --disable-gtk-doc-pdf
+
+GNUTLS_BUILD_SUBDIRS = gl lib extra
+
+define GNUTLS_BUILD_CMDS
+  for dirs in $(GNUTLS_BUILD_SUBDIRS) ; do \
+    $(TARGET_MAKE_ENV) $(GNUTLS_MAKE_ENV) $(GNUTLS_MAKE) $(GNUTLS_MAKE_OPT) -C $(@D)/$$dirs ; \
+  done
+endef
+
+define GNUTLS_INSTALL_STAGING_CMDS
+  for dirs in $(GNUTLS_BUILD_SUBDIRS) ; do \
+    $(TARGET_MAKE_ENV) $(GNUTLS_MAKE_ENV) $(GNUTLS_MAKE) $(GNUTLS_INSTALL_STAGING_OPT) -C $(@D)/$$dirs ; \
+  done
+endef
+
+define GNUTLS_INSTALL_TARGET_CMDS
+  for dirs in $(GNUTLS_BUILD_SUBDIRS) ; do \
+    $(TARGET_MAKE_ENV) $(GNUTLS_MAKE_ENV) $(GNUTLS_MAKE) $(GNUTLS_INSTALL_TARGET_OPT) -C $(@D)/$$dirs ; \
+  done
+endef
+
+define GNUTLS_BUILD_OPKG_CMDS
+  for dirs in $(GNUTLS_BUILD_SUBDIRS) ; do \
+    $(TARGET_MAKE_ENV) $(GNUTLS_MAKE_ENV) $(GNUTLS_MAKE) $(GNUTLS_BUILD_OPKG_OPT) -C $(@D)/$$dirs ; \
+  done
+endef
 
 $(eval $(call AUTOTARGETS,package,gnutls))
