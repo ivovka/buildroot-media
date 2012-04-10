b2dd7bff390d4e844f517e9f6d6396823b72ceb7
ivovka <vladimir_iva@pisem.net>
2012-04-10 14:36:05 +0400

    python: fix target build, reorganize code

--- package/python/python.mk
+++ package/python/python.mk
@@ -7,11 +7,15 @@ PYTHON_VERSION_MAJOR = 2.7
 PYTHON_VERSION       = $(PYTHON_VERSION_MAJOR).2
 PYTHON_SOURCE        = Python-$(PYTHON_VERSION).tar.bz2
 PYTHON_SITE          = http://python.org/ftp/python/$(PYTHON_VERSION)
+HOST_PYTHON_AUTORECONF = NO
+PYTHON_AUTORECONF = NO
+PYTHON_INSTALL_STAGING = YES
 PYTHON_BUILD_OPKG = YES
-
+HOST_PYTHON_DEPENDENCIES = host-zlib host-expat
+PYTHON_DEPENDENCIES  = host-python libffi
+PYTHON_OPKG_DEPENDENCIES = libffi,file
 PYTHON_SECTION = lang
 PYTHON_DESCRIPTION = The Python programming language
-PYTHON_OPKG_DEPENDENCIES = libffi,file
 
 # Python needs itself and a "pgen" program to build itself, both being
 # provided in the Python sources. So in order to cross-compile Python,
@@ -19,58 +23,40 @@ PYTHON_OPKG_DEPENDENCIES = libffi,file
 # installed in $(HOST_DIR), as it is needed when cross-compiling
 # third-party Python modules.
 
-HOST_PYTHON_DISABLED_MODULES = "readline _curses _curses_panel _tkinter nis gdbm bsddb _codecs_kr _codecs_jp _codecs_cn _codecs_tw _codecs_hk"
-
-HOST_PYTHON_CONF_OPT += 	\
-	--without-cxx-main 	\
-	--with-threads		\
-	--enable-unicode=ucs4	\
-	--with-system-expat
-
-HOST_PYTHON_MAKE_ENV = \
-	PYTHON_MODULES_INCLUDE=$(HOST_DIR)/usr/include \
-	PYTHON_MODULES_LIB="$(HOST_DIR)/lib $(HOST_DIR)/usr/lib" \
-	PYTHON_DISABLE_MODULES=$(HOST_PYTHON_DISABLED_MODULES)
-
-HOST_PYTHON_AUTORECONF = YES
-
-PYTHON_DEPENDENCIES  = zlib host-python libffi
-
-HOST_PYTHON_DEPENDENCIES = host-zlib host-expat
-
-PYTHON_INSTALL_STAGING = YES
-
 ifeq ($(BR2_PACKAGE_PYTHON_READLINE),y)
 PYTHON_DEPENDENCIES += readline
+PYTHON_OPKG_DEPENDENCIES += ,readline
 endif
 
 ifeq ($(BR2_PACKAGE_PYTHON_CURSES),y)
 PYTHON_DEPENDENCIES += ncurses
+PYTHON_OPKG_DEPENDENCIES += ,ncurses
 else
 PYTHON_CONF_OPT += --disable-curses
 endif
 
 ifeq ($(BR2_PACKAGE_PYTHON_PYEXPAT),y)
 PYTHON_DEPENDENCIES += expat
-PYTHON_CONF_OPT += --with-expat=system
-else
-PYTHON_CONF_OPT += --with-expat=none
+PYTHON_OPKG_DEPENDENCIES += ,expat
 endif
 
 ifeq ($(BR2_PACKAGE_PYTHON_BSDDB),y)
 PYTHON_DEPENDENCIES += berkeleydb
+PYTHON_OPKG_DEPENDENCIES += ,berkekeydb
 else
 PYTHON_CONF_OPT += --disable-bsddb
 endif
 
 ifeq ($(BR2_PACKAGE_PYTHON_SQLITE),y)
 PYTHON_DEPENDENCIES += sqlite
+PYTHON_OPKG_DEPENDENCIES += ,sqlite
 else
 PYTHON_CONF_OPT += --disable-sqlite3
 endif
 
 ifeq ($(BR2_PACKAGE_PYTHON_SSL),y)
 PYTHON_DEPENDENCIES += openssl
+PYTHON_OPKG_DEPENDENCIES += ,openssl
 else
 PYTHON_CONF_OPT += --disable-ssl
 endif
@@ -79,61 +65,75 @@ ifneq ($(BR2_PACKAGE_PYTHON_CODECSCJK),y)
 PYTHON_CONF_OPT += --disable-codecs-cjk
 endif
 
-ifneq ($(BR2_PACKAGE_PYTHON_UNICODEDATA),y)
-PYTHON_CONF_OPT += --disable-unicodedata
-endif
-
 ifeq ($(BR2_PACKAGE_PYTHON_BZIP2),y)
 PYTHON_DEPENDENCIES += bzip2
+PYTHON_OPKG_DEPENDENCIES += ,bzip2
 else
 PYTHON_CONF_OPT += --disable-bz2
 endif
 
 ifeq ($(BR2_PACKAGE_PYTHON_ZLIB),y)
 PYTHON_DEPENDENCIES += zlib
+PYTHON_OPKG_DEPENDENCIES += ,zlib
 else
 PYTHON_CONF_OPT += --disable-zlib
 endif
 
-PYTHON_CONF_ENV += \
-	PYTHON_FOR_BUILD=$(HOST_PYTHON_DIR)/python \
-	PGEN_FOR_BUILD=$(HOST_PYTHON_DIR)/Parser/pgen \
-	ac_cv_have_long_long_format=yes
+PYTHON_DISABLED_MODULES = "readline _curses _curses_panel _tkinter nis gdbm bsddb _codecs_kr _codecs_jp _codecs_cn _codecs_tw _codecs_hk"
 
+PYTHON_CONF_ENV += \
+	ac_cv_file_dev_ptc=no \
+	ac_cv_file_dev_ptmx=yes \
+	ac_cv_func_lchflags_works=no \
+	ac_cv_func_chflags_works=no \
+	ac_cv_func_printf_zd=yes \
+	OPT="$(TARGET_CFLAGS) -fno-strict-aliasing" \
+	PYTHON_FOR_BUILD=$(HOST_PYTHON_DIR)/python \
+	PGEN_FOR_BUILD=$(HOST_PYTHON_DIR)/Parser/pgen
+
+
+#	ac_cv_have_long_long_format=yes
 PYTHON_CONF_OPT += \
-	--without-cxx-main 	\
-	--without-doc-strings	\
-	--with-system-ffi	\
-	--disable-pydoc		\
-	--disable-test-modules	\
-	--disable-lib2to3	\
-	--disable-gdbm		\
-	--disable-tk		\
-	--disable-nis		\
-	--disable-dbm		\
-	--with-threads		\
+	--with-threads \
 	--enable-unicode=ucs4	\
+	--disable-ipv6 \
+	--disable-profiling \
+	--without-pydebug \
+	--without-doc-strings \
 	--without-tsc		\
 	--without-pymalloc	\
 	--without-fpectl	\
 	--without-wctype-functions \
-	--without-pydebug \
-	--without-doc-strings
+	--without-cxx-main 	\
+	--with-system-ffi	\
+	--with-system-expat
 
 PYTHON_MAKE_ENV = \
-	PYTHON_MODULES_INCLUDE=$(STAGING_DIR)/usr/include \
+	PYTHON_MODULES_INCLUDE="$(STAGING_DIR)/usr/include" \
 	PYTHON_MODULES_LIB="$(STAGING_DIR)/lib $(STAGING_DIR)/usr/lib"
+
+PYTHON_MAKE_OPT = \
+	HOSTPYTHON=$(HOST_PYTHON_DIR)/python \
+	HOSTPGEN=$(HOST_PYTHON_DIR)/Parser/pgen \
+	PYTHON_DISABLE_MODULES=$(PYTHON_DISABLED_MODULES)
+
+PYTHON_INSTALL_STAGING_OPT += $(PYTHON_MAKE_OPT)
+PYTHON_INSTALL_STAGING_OPT += DESTDIR=$(STAGING_DIR) install
+
+PYTHON_INSTALL_TARGET_OPT += $(PYTHON_MAKE_OPT)
+PYTHON_INSTALL_TARGET_OPT += DESTDIR=$(TARGET_DIR) install
 
 define PYTHON_REMOVE_AUTOCONF_CHECK
 	sed -e "/version_required(2\.65)/d" -i $(PYTHON_DIR)/configure.in
 endef
 
 PYTHON_POST_PATCH_HOOKS += PYTHON_REMOVE_AUTOCONF_CHECK
-define HOST_PYTHON_REMOVE_AUTOCONF_CHECK
-	sed -e "/version_required(2\.65)/d" -i $(HOST_PYTHON_DIR)/configure.in
+
+define PYTHON_FIX_MAKEFILE_STAGING
+  sed -e "s|^LIBDIR=.*|LIBDIR= $(STAGING_DIR)/usr/lib|" -i $(STAGING_DIR)/usr/lib/python*/config/Makefile
 endef
 
-HOST_PYTHON_POST_PATCH_HOOKS += HOST_PYTHON_REMOVE_AUTOCONF_CHECK
+PYTHON_POST_INSTALL_STAGING_HOOKS += PYTHON_FIX_MAKEFILE_STAGING
 #
 # Development files removal
 #
@@ -146,15 +146,9 @@ define PYTHON_REMOVE_DEVFILES_TARGET
   $(call PYTHON_REMOVE_DEVFILES,$(TARGET_DIR))
 endef
 
-define PYTHON_REMOVE_DEVFILES_OPKG
-  $(call PYTHON_REMOVE_DEVFILES,$(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME))
-endef
-
 ifneq ($(BR2_HAVE_DEVFILES),y)
 PYTHON_POST_INSTALL_TARGET_HOOKS += PYTHON_REMOVE_DEVFILES_TARGET
 endif
-
-PYTHON_PRE_BUILD_OPKG_HOOKS += PYTHON_REMOVE_DEVFILES_OPKG
 
 #
 # Remove useless files. In the config/ directory, only the Makefile
@@ -169,10 +163,6 @@ endef
 
 define PYTHON_REMOVE_USELESS_FILES_TARGET
   $(call PYTHON_REMOVE_USELESS_FILES,$(TARGET_DIR))
-endef
-
-define PYTHON_REMOVE_USELESS_FILES_OPKG
-  $(call PYTHON_REMOVE_USELESS_FILES,$(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME))
 endef
 
 define PYTHON_REMOVE_USELESS_MODULES
@@ -190,23 +180,62 @@ define PYTHON_REMOVE_USELESS_MODULES_TARGET
   $(call PYTHON_REMOVE_USELESS_MODULES,$(TARGET_DIR))
 endef
 
-define PYTHON_REMOVE_USELESS_MODULES_OPKG
-  $(call PYTHON_REMOVE_USELESS_MODULES,$(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME))
-endef
-
 PYTHON_POST_INSTALL_TARGET_HOOKS += PYTHON_REMOVE_USELESS_FILES_TARGET
 PYTHON_POST_INSTALL_TARGET_HOOKS += PYTHON_REMOVE_USELESS_MODULES_TARGET
-PYTHON_PRE_BUILD_OPKG_HOOKS += PYTHON_REMOVE_USELESS_FILES_OPKG
-PYTHON_PRE_BUILD_OPKG_HOOKS += PYTHON_REMOVE_USELESS_MODULES_OPKG
+
+PYTHON_BUILD_OPKG_INC_DIRS = compiler ctypes ctypes/macholib distutils distutils/command email
+PYTHON_BUILD_OPKG_INC_DIRS +=  email/mime encodings hotshot importlib json logging
+PYTHON_BUILD_OPKG_INC_DIRS +=  multiprocessing sqlite3 xml xml/dom xml/etree
+PYTHON_BUILD_OPKG_INC_DIRS +=  xml/parsers xml/sax
+
+define PYTHON_BUILD_OPKG_CMDS
+  mkdir -p $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/bin
+  cp $(@D)/python $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/bin/
+  mkdir -p $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib
+  cp -PR $(@D)/libpython2.7.so.1.0 $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib/
+  ln -sf libpython2.7.so.1.0 $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib/libpython2.7.so
+  mkdir -p $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib/python$(PYTHON_VERSION_MAJOR)/config
+  cp $(@D)/Makefile $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib/python$(PYTHON_VERSION_MAJOR)/config
+  cp $(@D)/pyconfig.h $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib/python$(PYTHON_VERSION_MAJOR)/config
+  mkdir -p $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib/python$(PYTHON_VERSION_MAJOR)/lib-dynload
+  cp $(@D)/build/lib*/*.so $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib/python$(PYTHON_VERSION_MAJOR)/lib-dynload
+  cp $(@D)/Lib/*.py $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib/python$(PYTHON_VERSION_MAJOR)
+  mkdir -p $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib/python$(PYTHON_VERSION_MAJOR)/plat-linux2
+  cp $(@D)/Lib/plat-linux2/* $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib/python$(PYTHON_VERSION_MAJOR)/plat-linux2
+  for dirs in $(PYTHON_BUILD_OPKG_INC_DIRS) ; do \
+    (mkdir -p $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib/python$(PYTHON_VERSION_MAJOR)/$$dirs && \
+    cp $(@D)/Lib/$$dirs/*.py $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib/python$(PYTHON_VERSION_MAJOR)/$$dirs) \
+  done 
+  mkdir -p $(BUILD_DIR_OPKG)/$(PYTHON_BASE_NAME)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages
+endef
+
+HOST_PYTHON_DISABLED_MODULES = "readline _curses _curses_panel _tkinter nis gdbm bsddb _codecs_kr _codecs_jp _codecs_cn _codecs_tw _codecs_hk"
+
+HOST_PYTHON_CONF_OPT += 	\
+	--without-cxx-main 	\
+	--with-threads		\
+	--enable-unicode=ucs4	\
+	--with-system-expat
+
+HOST_PYTHON_MAKE_ENV = \
+	PYTHON_MODULES_INCLUDE=$(HOST_DIR)/usr/include \
+	PYTHON_MODULES_LIB="$(HOST_DIR)/lib $(HOST_DIR)/usr/lib" \
+	PYTHON_DISABLE_MODULES=$(HOST_PYTHON_DISABLED_MODULES)
+
+define HOST_PYTHON_REMOVE_AUTOCONF_CHECK
+	sed -e "/version_required(2\.65)/d" -i $(HOST_PYTHON_DIR)/configure.in
+endef
+
+HOST_PYTHON_POST_PATCH_HOOKS += HOST_PYTHON_REMOVE_AUTOCONF_CHECK
 
 define HOST_PYTHON_LN_PYTHON2
     ln -sf python $(HOST_DIR)/usr/bin/python2
     cp $(HOST_PYTHON_DIR)/Parser/pgen $(HOST_DIR)/usr/bin/
+    sed -e "s:%PREFIX%:$(STAGING_DIR)/usr:g" -e "s:%CFLAGS%:$(TARGET_CFLAGS):g" \
+        $(TOPDIR)/package/python/python-config > $(HOST_DIR)/usr/bin/python2.7-config
 endef
 
 HOST_PYTHON_POST_INSTALL_HOOKS += HOST_PYTHON_LN_PYTHON2
-
-PYTHON_AUTORECONF = YES
 
 $(eval $(call AUTOTARGETS,package,python))
 $(eval $(call AUTOTARGETS,package,python,host))
