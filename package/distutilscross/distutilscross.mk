4cd5ebc4d7becb5de418262f6ac3551215beadac
ivovka <vladimir_iva@pisem.net>
2012-04-12 12:10:51 +0400

    distutilscross: dont build for target, - it is required only on host

--- package/distutilscross/distutilscross.mk
+++ package/distutilscross/distutilscross.mk
@@ -7,14 +7,13 @@ DISTUTILSCROSS_VERSION = 0.1
 DISTUTILSCROSS_SOURCE = distutilscross-$(DISTUTILSCROSS_VERSION).tar.gz
 DISTUTILSCROSS_SITE = http://pypi.python.org/packages/source/d/distutilscross
 DISTUTILSCROSS_INSTALL_STAGING = NO
-DISTUTILSCROSS_INSTALL_TARGET = YES
+DISTUTILSCROSS_INSTALL_TARGET = NO
 DISTUTILSCROSS_BUILD_OPKG = NO
-DISTUTILSCROSS_DEPENDENCIES = host-python python distribute
-#DISTUTILSCROSS_CONF_ENV = 
-#DISTUTILSCROSS_CONF_OPT = 
-define DISTUTILSCROSS_BUILD_CMDS
+HOST_DISTUTILSCROSS_DEPENDENCIES = host-python host-distribute
+define HOST_DISTUTILSCROSS_BUILD_CMDS
     (cd $(@D) && \
     $(HOST_MAKE_ENV) python setup.py install --prefix=$(HOST_DIR)/usr \
     )
 endef
 $(eval $(call GENTARGETS,package,distutilscross))
+$(eval $(call GENTARGETS,package,distutilscross,host))
