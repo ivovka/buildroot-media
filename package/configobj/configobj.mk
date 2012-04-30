9fc3cee31fc5610d0df1f26024c620476c01611e
Vladimir Ivakin <vladimir_iva@pisem.net>
2012-05-01 01:02:26 +0400

    configobj: Add host-distutilscross dependency

--- package/configobj/configobj.mk
+++ package/configobj/configobj.mk
@@ -12,7 +12,7 @@ CONFIGOBJ_BUILD_OPKG = YES
 CONFIGOBJ_SECTION = python
 CONFIGOBJ_DESCRIPTION = a simple but powerful config file reader and writer
 CONFIGOBJ_OPKG_DEPENDENCIES = python
-CONFIGOBJ_DEPENDENCIES = host-python python distribute distutilscross
+CONFIGOBJ_DEPENDENCIES = host-python python distribute distutilscross host-distutilscross
 
 CONFIGOBJXCPREFIX=$(STAGING_DIR)/usr
 CONFIGOBJ_LDFLAGS="$(TARGET_LDFLAGS) -L$(STAGING_DIR)/usr/lib -L$(STAGING_DIR)/lib"
