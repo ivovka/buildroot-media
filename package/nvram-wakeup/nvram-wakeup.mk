f27cceaa1cdf6960d0fbd14ca5b74fab92e3f8f1
Vladimir Ivakin <vladimir_iva@pisem.net>
2012-05-01 01:10:27 +0400

    nvram-wakeup: fix build

--- package/nvram-wakeup/nvram-wakeup.mk
+++ package/nvram-wakeup/nvram-wakeup.mk
@@ -16,22 +16,24 @@ NVRAM_WAKEUP_DESCRIPTION = This is a small program that reads and writes the Wak
 
 define NVRAM_WAKEUP_BUILD_CMDS
     $(TARGET_MAKE_ENV) \
-    $(MAKE) -C $(@D) CC="$(TARGET_CC)" $(TARGET_MAKE_ENV)
+    $(MAKE) -C $(@D) CC="$(TARGET_CC)" SIZE="$(TARGET_CROSS)size" $(TARGET_MAKE_ENV)
 endef
 
 define NVRAM_WAKEUP_INSTALL_TARGET_CMDS
     $(TARGET_MAKE_ENV) \
-    $(MAKE) prefix=$(TARGET_DIR)/usr -C $(@D) install
+    $(MAKE) prefix=$(TARGET_DIR)/usr -C $(@D) CC="$(TARGET_CC)" SIZE="$(TARGET_CROSS)size" install
     mkdir -p $(TARGET_DIR)/etc
     cp $(NVRAM_WAKEUP_DIR_PREFIX)/$(NVRAM_WAKEUP_NAME)/nvram-wakeup.conf $(TARGET_DIR)/etc
 endef
 
 define NVRAM_WAKEUP_BUILD_OPKG_CMDS
     $(TARGET_MAKE_ENV) \
-    $(MAKE) prefix=$(BUILD_DIR_OPKG)/nvram-wakeup-$(NVRAM_WAKEUP_VERSION)/usr -C $(@D) install
+    $(MAKE) prefix=$(BUILD_DIR_OPKG)/nvram-wakeup-$(NVRAM_WAKEUP_VERSION)/usr -C $(@D) CC="$(TARGET_CC)" SIZE="$(TARGET_CROSS)size" install
     mkdir -p $(BUILD_DIR_OPKG)/nvram-wakeup-$(NVRAM_WAKEUP_VERSION)/etc
     cp $(NVRAM_WAKEUP_DIR_PREFIX)/$(NVRAM_WAKEUP_NAME)/nvram-wakeup.conf $(BUILD_DIR_OPKG)/nvram-wakeup-$(NVRAM_WAKEUP_VERSION)/etc
-    rm -rf $(BUILD_DIR_OPKG)/nvram-wakeup-$(NVRAM_WAKEUP_VERSION)/usr/share
+    rm -rf $(BUILD_DIR_OPKG)/$(NVRAM_WAKEUP_BASE_NAME)/usr/share
+    rm -rf $(BUILD_DIR_OPKG)/$(NVRAM_WAKEUP_BASE_NAME)/usr/sbin
+    rm $(BUILD_DIR_OPKG)/$(NVRAM_WAKEUP_BASE_NAME)/usr/bin/vdrshutdown
 endef
 
 $(eval $(call GENTARGETS,package,nvram-wakeup))
