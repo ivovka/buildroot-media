f525e2a07e0e32e2f870c60ef7275a6cc74503a5
Vladimir Ivakin <vladimir_iva@pisem.net>
2012-05-18 01:47:29 +0400

    a try to build LADSPA plugins to normalize volume in XBMC

---
+++ package/multimedia/swh-plugins/swh-plugins.mk
@@ -0,0 +1,24 @@
+#############################################################
+#
+# swh-plugins
+#
+#############################################################
+SWH_PLUGINS_VERSION = 0.4.15
+SWH_PLUGINS_SOURCE = swh-plugins-$(SWH_PLUGINS_VERSION).tar.gz
+SWH_PLUGINS_SITE = http://plugin.org.uk/releases/$(SWH_PLUGINS_VERSION)
+SWH_PLUGINS_INSTALL_STAGING = NO
+SWH_PLUGINS_BUILD_OPKG = YES
+
+SWH_PLUGINS_SECTION = audio
+SWH_PLUGINS_DESCRIPTION = LADSPA plugins
+SWH_PLUGINS_OPKG_DEPENDENCIES = alsa-lib
+SWH_PLUGINS_DEPENDENCIES = alsa-lib
+
+SWH_PLUGINS_CONF_OPT += --localstatedir=/var \
+	--disable-nls \
+	--enable-sse \
+	--without-libiconv-prefix \
+	--without-libintl-prefix \
+
+
+$(eval $(call AUTOTARGETS,package/multimedia,swh-plugins))
